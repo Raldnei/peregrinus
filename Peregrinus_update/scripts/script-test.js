05bb6a81027e3bbd28397a13204545a41ca16e46
Raldnei Miguel <146400289+Raldnei@users.noreply.github.com>
2024-07-24 15:57:31 -0300

    none

--- Peregrinus_update/scripts/script-test.js
+++ Peregrinus_update/scripts/script-test.js
@@ -3,77 +3,59 @@ function loadHeader() {
     const headerHTML = `
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <!-- MUDAR ESSA CONFIG PARA UM STYLE.CSS-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <title>Document</title>
+   
     <style>
-        body{
-           
-            color-scheme: dark
+        body {
+            color-scheme: dark;
         }
         #logo a {
             text-decoration: none;
             color: inherit;
         }
-       /* configuração do search */ 
         .search-form {
             display: flex;
             align-items: center; 
             width: 100%; 
             max-width: 600px;
             margin: 0 auto; 
-            min-width: 20%;
+            min-width: 15%;
         }
-        /* faz um grupo (search + botão + loja)*/
         .input-group {
             display: flex;
             align-items: center; 
             flex-grow: 1; 
         }
-        /*search*/
         .input-group input {
             flex-grow: 1; 
         }
-        /* config do nome LOJA */
         .nav-link-loja {
-            margin-left: 500px; /* Adiciona margem à esquerda do link "LOJA" */
+            margin-left: 500px;
             display: flex;
             align-items: center;
             color: #00AFEF;
         }
-
-        .user-icon {
-            font-size: 20px; /* tamanho do ícone */
-            height: 25px; /* altura  ícone */
-            width: 25px; /* largura  ícone */
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border-radius: 50%; 
-            background-color: #f0f0f0; 
-            color: #333; 
-            border: 2px solid #ddd; /* Borda ao redor do ícone */
-            margin: 0 10px; 
-        }
-        .fa-shopping-cart{
+        .navbar-nav .nav-link {
             color: #00AFEF;
         }
-        .btn-outline-primary {
-            color: #00AFEF;
-            border-color: #00AFEF;
-        }
-        .btn-outline-primary:hover{
-            background-color: #f0f0f0;
-            color: #00AFEF;
-        }
-        .fa-shopping-cart:hover{
+        .navbar-nav .nav-link:hover {
             color: #ffffff;
         }
-        .nav-link-loja:hover{
-            color: #ffffff;
+        .close-btn {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            background: none;
+            border: none;
+            color: #fff;
+            font-size: 24px;
+            cursor: pointer;
         }
-
-        /*telas diferentes*/
+        .close-btn:hover {
+            color: #ccc;
+        }
         @media only screen and (max-width: 500px) {
             .container-fluid {
                 flex-direction: column; 
@@ -89,45 +71,77 @@ function loadHeader() {
                 width: 100%; 
                 justify-content: center; 
             }
-            .fa-shopping-cart, .nav-link-loja, .user-icon{
+            .nav-link-loja {
                 display: none;
             }
-            
+            .navbar-nav {
+                flex-direction: column;
+                width: 100%;
+                text-align: center;
+            }
+            .navbar-nav .nav-link {
+                margin: 10px 0;
+            }
         }
-    </style><!-- FIM MUDAR ESSA CONFIG PARA UM STYLE.CSS-->
+    </style>
 </head>
 <body>
-    <div id="header" class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid d-flex justify-content-between align-items-center">
             <!-- Logo -->
             <div id="logo">
                 <a href="#">
-                    <img src="../images/logo_azul_final.svg" alt="Logo" height="40">
-                </a>
+                    <img src="../images/logo_azul_final.svg" alt="Logo" height="75">
+                </a><button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span><!-- Botão de colapso do menu (hamburger menu) -->
+                </button>
             </div>
+ 
+            <!-- Menu colapsável -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="d-flex flex-column flex-lg-row w-100">
 
-            <!-- Search Form -->
-            <form class="search-form" action="buscar.py">
-                <div class="d-flex align-items-center justify-content-between input-group">
-                    <input type="text" id="pesquisa" class="form-control" placeholder="Buscar">
-                    <button class="btn btn-outline-primary" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                    <a class="nav-link nav-link-loja px-3" href="#">LOJA</a>
+                    <!-- Search Form -->
+                    <form class="search-form me-auto d-none d-lg-flex" action="buscar.py">
+                        <div class="d-flex align-items-center justify-content-between input-group">
+                            <input type="text" id="pesquisa" class="form-control" placeholder="Buscar">
+                            <button class="btn btn-outline-primary" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <a class="nav-link nav-link-loja px-3" href="#">LOJA</a>
+                        </div>
+                    </form>
+
+                    <!-- Links de navegação -->
+                    <div class="navbar-nav ms-auto d-none d-lg-flex">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-shopping-cart"></i>
+                        </a>
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-user"></i>
+                        </a>
+                    </div>
+
+                    <!-- TELA MENOR -->
+                    <div class="navbar-nav ms-auto d-lg-none">
+                        <form class="search-form me-auto d-lg-flex" action="buscar.py">
+                            <div class="d-flex align-items-center justify-content-between input-group">
+                                <input type="text" id="pesquisa" class="form-control" placeholder="Buscar">
+                                <button class="btn btn-outline-primary" type="submit">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                
+                            </div>
+                        </form>
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-shopping-cart"></i> Meus Pedidos </a>
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-user"></i> Meu Perfil </a>
+                    </div>
                 </div>
-            </form>
-
-            <!-- Right Side Icons -->
-            <div class="d-flex align-items-center">
-                <a class="nav-link" href="#">
-                    <i class="container fas fa-shopping-cart"></i>
-                </a>
-                <a class="nav-link" href="#">
-                    <i class="fas fa-user user-icon"></i>
-                </a>
             </div>
         </div>
-    </div>
+    </nav>
 
 
     `;
