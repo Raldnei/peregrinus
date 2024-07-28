e9cf0b4fd13d782b8b07716ffd9cba2ac338e7ee
Raldnei Miguel <146400289+Raldnei@users.noreply.github.com>
2024-07-28 20:04:46 -0300

    update script do header

--- Peregrinus_update/scripts/script-test.js
+++ Peregrinus_update/scripts/script-test.js
@@ -5,6 +5,7 @@ function loadHeader() {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    
     <title>Document</title>
    
     <style>
