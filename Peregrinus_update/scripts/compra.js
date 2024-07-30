29e50203c7458da38878bb40009f0cf31ddb1600
Messias-Acacy <130923301+Messias-Acacy@users.noreply.github.com>
2024-07-30 15:19:15 -0300

    Add files via upload

--- Peregrinus_update/scripts/compra.js
+++ Peregrinus_update/scripts/compra.js
@@ -1,26 +1,51 @@
-function contador1(){
-    let num = 0
-    let num1 = num--
-    return num1
-}
-function contador2(){
-    let num = 0
-    let num1 = num++
-    return num1
-}
-console.log(num1)
-document.getElementsByClassName('contador').innerHTML = 'quantidade: ' + num1
-document.getElementsByClassName('contador').innerHTML = 'quantidade: ' + num1
 
-let preco = 59.00
-let quantidade = 1*num1
-let total = (preco*quantidade) + frete
-let frete = 9.90
-let desconto = 0.00
-console.log(total)
-console.log(frete)
-console.log(desconto)
-document.getElementById('preco-total').innerHTML = 'Total de Produtos: R$' + total
-document.getElementById('frete').innerHTML = 'Frete: R$' + frete
-document.getElementById('Desconto').innerHTML = 'Desconto: ' + desconto
-document.getElementById('pagamento').innerHTML = 'Total a pagar : R$' + total
+
+function contador(){
+    let arraydiv = Array.from(document.getElementById('conteudo-divisao').children);
+    arraydiv =arraydiv.slice(0,arraydiv.length-3);
+    let precoTotal = 0
+    let totalprodutos = 0
+
+    for (let div of arraydiv) {
+
+        let elementoClicado = event.target.parentNode;
+        let elementoAtual = String(elementoClicado.parentNode.id)
+
+        if(div.id == elementoAtual){
+       
+            let element = String(event.target.classList).split(' ')[String(event.target.classList).split(' ').length-1]
+
+   
+            if(element == 'botao-menos'){
+                document.getElementById(div.id).getElementsByTagName('p')[1].textContent = Number(document.getElementById(div.id).getElementsByTagName('p')[1].textContent)-1
+
+                if(Number(document.getElementById(div.id).getElementsByTagName('p')[1].textContent) < 0)
+                    document.getElementById(div.id).getElementsByTagName('p')[1].textContent = 0
+      
+
+            }
+            
+            if(element == 'botao-mais'){
+                document.getElementById(div.id).getElementsByTagName('p')[1].textContent = Number(document.getElementById(div.id).getElementsByTagName('p')[1].textContent)+1
+                }
+
+        }
+        precoTotal+=Number(document.getElementById(div.id).getElementsByTagName('p')[0].textContent)*Number(document.getElementById(div.id).getElementsByTagName('p')[1].textContent)
+        totalprodutos +=Number(document.getElementById(div.id).getElementsByTagName('p')[1].textContent)
+    }
+
+    if(totalprodutos > 10){
+        document.getElementById('Desconto').textContent='Desconto: R$ 20,00'
+    }
+    else if(totalprodutos > 5){
+        document.getElementById('Desconto').textContent='Desconto: R$ 10,00'
+    }
+    else{
+        document.getElementById('Desconto').textContent='Desconto: R$ 5,00'
+    }
+
+    document.getElementById('preco-total').textContent = 'Total de Produtos: '+totalprodutos
+    document.getElementById('pagamento').textContent = 'total a pagar: R$' + precoTotal
+
+
+} 
