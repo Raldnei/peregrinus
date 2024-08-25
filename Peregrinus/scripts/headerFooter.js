46c31fcee6934b8d210730cc0294949dccace97a
Raldnei Miguel <146400289+Raldnei@users.noreply.github.com>
2024-08-24 21:32:58 -0300

    updates
    estilização, root, responsividade de algumas paginas

--- Peregrinus/scripts/headerFooter.js
+++ Peregrinus/scripts/headerFooter.js
@@ -1,6 +1,7 @@
 function loadHeader() {
 
     const headerHTML = `
+    <link rel="stylesheet" href="../styles/global.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
@@ -9,9 +10,10 @@ function loadHeader() {
     <title>Document</title>
    
     <style>
-        body {
-            color-scheme: dark;
+        #header {
+             background-color: var(--cor-header-background-color);
         }
+             
         #logo a {
             text-decoration: none;
             color: inherit;
@@ -19,7 +21,7 @@ function loadHeader() {
         .search-form {
             display: flex;
             align-items: center; 
-            width: 100%; 
+            width: 100%;
             max-width: 600px;
             margin: 0 auto; 
             min-width: 15%;
@@ -36,16 +38,16 @@ function loadHeader() {
             margin-left: 500px;
             display: flex;
             align-items: center;
-            color: #00AFEF;
+            color: var(--cor-header-items-color);
         }
             .nav-link-loja:hover{
-            color: #ffffff;
+            color: var(--cor-hover-header);
         }
         .navbar-nav .nav-link {
-            color: #00AFEF;
+            color: var(--cor-header-items-color);
         }
         .navbar-nav .nav-link:hover {
-            color: #ffffff;
+            color: var(--cor-hover-header);
         }
         .close-btn {
             position: absolute;
@@ -53,14 +55,14 @@ function loadHeader() {
             right: 10px;
             background: none;
             border: none;
-            color: #fff;
+            color: var(--cor-hover-header);
             font-size: 24px;
             cursor: pointer;
         }
         .close-btn:hover {
             color: #ccc;
         }
-        @media only screen and (max-width: 500px) {
+        @media only screen and (max-width: 767px) {
             .container-fluid {
                 flex-direction: column; 
                 align-items: center; 
@@ -86,28 +88,45 @@ function loadHeader() {
             .navbar-nav .nav-link {
                 margin: 10px 0;
             }
+            .navbar-toggler-icon {
+            background: none;
+            color: var(--cor-header-items-color);
         }
+            }
+        @media (min-width: 769px) {
+    .navbar-nav {
+        display: flex;
+        flex-direction: row;
+    }
+    .navbar-nav .nav-link {
+        margin: 0 10px; /* Ajusta o espaçamento entre os itens da navbar */
+    }
+}
+
+           
     </style>
 </head>
 <body>
-    <nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
+    <nav id="header" class="navbar navbar-expand-md navbar">
+        <div class="container-fluid d-flex justify-content-center align-items-center">
             <!-- Logo -->
             <div id="logo">
                 <a href="../pages/index.html">
                     <img src="../images/logo_azul_final.svg" alt="Logo" height="75">
-                </a><button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span><!-- Botão de colapso do menu (hamburger menu) -->
+                </a>
+                <!-- Botão do Menu (Hamburger) -->
+                <button class="navbar-toggler d-md-none ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa fa-bars"></span><!-- Botão de colapso do menu (hamburger menu) -->
                 </button>
             </div>
- 
-            <!-- Menu colapsável -->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="d-flex flex-column flex-lg-row w-100">
 
-                    <!-- Search Form -->
-                    <form class="search-form me-auto d-none d-lg-flex" action="buscar.py">
-                        <div class="d-flex align-items-center justify-content-between input-group">
+            <!-- Menu Colapsável -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="d-flex flex-lg-row align-items-center w-100">
+
+                    <!-- Formulário de Pesquisa (Aparece em telas médias e maiores) -->
+                    <form class="search-form d-none d-md-flex me-auto" action="buscar.py">
+                        <div class="d-flex align-items-center justify-content-center input-group">
                             <input type="text" id="pesquisa" class="form-control" placeholder="Buscar">
                             <button class="btn btn-outline-primary" type="submit">
                                 <i class="fas fa-search"></i>
@@ -116,8 +135,8 @@ function loadHeader() {
                         </div>
                     </form>
 
-                    <!-- Links de navegação -->
-                    <div class="navbar-nav ms-auto d-none d-lg-flex">
+                    <!-- Links de Navegação (Aparecem em telas médias e maiores) -->
+                    <div class="navbar-nav ms-auto d-none d-md-flex">
                         <a class="nav-link" href="../pages/compra.html">
                             <i class="fas fa-shopping-cart"></i>
                         </a>
@@ -126,26 +145,35 @@ function loadHeader() {
                         </a>
                     </div>
 
-                    <!-- TELA MENOR -->
-                    <div class="navbar-nav ms-auto d-lg-none">
-                        <form class="search-form me-auto d-lg-flex" action="buscar.py">
-                            <div class="d-flex align-items-center justify-content-between input-group">
+                    <!-- Menu para Telas Menores (Aparece em telas menores que 768px) -->
+                    <div class="navbar-nav ms-auto d-md-none">
+                        <form class="search-form d-flex me-auto" action="buscar.py">
+                            <div class="d-flex align-items-center justify-content-center input-group">
                                 <input type="text" id="pesquisa" class="form-control" placeholder="Buscar">
                                 <button class="btn btn-outline-primary" type="submit">
                                     <i class="fas fa-search"></i>
                                 </button>
-                                
                             </div>
                         </form>
                         <a class="nav-link" href="../pages/meus_pedidos.html">
-                            <i class="fas fa-shopping-cart"></i> Meus Pedidos </a>
+                            <i class="fas fa-shopping-cart"></i> Meus Pedidos
+                        </a>
                         <a class="nav-link" href="../pages/menu.html">
-                            <i class="fas fa-user"></i> Meu Perfil </a>
+                            <i class="fas fa-user"></i> Meu Perfil
+                        </a>
+                        <a class="nav-link" href="../pages/menu.html">
+                            <i class="fas fa-shopping-bag"></i> Loja
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </nav>
+
+
+
+
+
 
 
     `;
@@ -160,10 +188,10 @@ function loadFooter() {
      <style>
        
         footer { /* Footer completinho */
-            background-color: #ffffff;
-            color: #fff;
+            background-color: var(--cor-footer-background-color);
            
-      
+          
+            
         }
         .footer-content { /* Footer completão */
             display: flex;
@@ -179,10 +207,10 @@ function loadFooter() {
         .icons i{  /* Icones do cartão */
             margin: 0 10px;
             font-size: 36px; 
-            color: #00AFEF;
+            color: var(--cor-footer-items-color);
         }
         .icons p{
-            color: #00AFEF;
+            color: var(--cor-footer-items-color);
         }
         .footer-links a { /* Links lado Direito */
             color: #00AFEF;
@@ -196,7 +224,7 @@ function loadFooter() {
         }
         .copyright {
             font-size: 12px;
-            color: #00AFEF;
+            color: var(--cor-footer-items-color);
         }
         /* Resolução menor */
         @media (max-width: 768px) {
@@ -225,7 +253,7 @@ function loadFooter() {
     </style>
 </head>
 <body>
-    <footer class="bg-dark">
+    <footer class="">
         <div class="footer-content container d-flex">
             <!-- Lado Esquerdo: Logo e Copyright -->
             <div class="logo"><!-- O logo é o nome da classe -->
@@ -234,7 +262,7 @@ function loadFooter() {
             </div>
 
             <!-- Meio: Título e Ícones de Cartões -->
-            <div class="icons text-center"> <!-- O icones é o nome da classe -->
+            <div class="icons text-center"> <!-- O icons é o nome da classe -->
                 <p>Aceitamos os cartões</p>
                 <i class="fab fa-cc-visa"></i>
                 <i class="fab fa-cc-mastercard"></i>
