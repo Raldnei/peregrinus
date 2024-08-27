a074997a1afef9e655ee2bb55c474396e36c7fef
Raldnei Miguel <146400289+Raldnei@users.noreply.github.com>
2024-08-27 15:01:48 -0300

    teste

--- Peregrinus/scripts/headerFooter.js
+++ Peregrinus/scripts/headerFooter.js
@@ -1,7 +1,7 @@
 function loadHeader() {
 
     const headerHTML = `
-    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="Peregrinus/styles/global.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
